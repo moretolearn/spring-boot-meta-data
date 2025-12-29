649ac840ed6216fb4bdbcf138d45165d46a830fe
moretolearn <yanumularammohan661@gmail.com>
2025-12-29 21:16:04 +0530

    added package name

--- src/main/java/com/moretolearn/controller/MetadataController.java
+++ src/main/java/com/moretolearn/controller/MetadataController.java
@@ -1,4 +1,4 @@
-package com.moretolearn;
+package com.moretolearn.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
